0e52c89e634ea02a23302b7874cf703a9dbfd4f9
PhongLe <phongle@phongles-macbook-pro.local>
2017-03-31 07:39:16 +0700

    Initial Commit

---
+++ Swipe Navigation DemoTests/Swipe_Navigation_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swipe_Navigation_DemoTests.swift
+//  Swipe Navigation DemoTests
+//
+//  Created by PhongLe on 3/31/17.
+//  Copyright © 2017 PhongLe. All rights reserved.
+//
+
+import XCTest
+@testable import Swipe_Navigation_Demo
+
+class Swipe_Navigation_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
